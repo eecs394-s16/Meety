2536332240634faa812ad1a5a6759c5297480e0f
jnbohrer <jbohrer1@mac.com>
2016-04-04 19:53:49 -0500

    Updated form and controller

--- app/example/scripts/SendingFormController.js
+++ app/example/scripts/SendingFormController.js
@@ -1,11 +1,13 @@
 angular
   .module('example')
   .controller('SendingFormController', function($scope, supersonic) {
+  	$scope.master = {};
 
     $scope.navbarTitle = "Learn More";
     this.hello = "some other string";
+
     this.submit = function (){
       //on click or on submit function
-      supersonic.logger.debug("Submitted form");
+      supersonic.logger.debug($scope.master);
     };
   });
