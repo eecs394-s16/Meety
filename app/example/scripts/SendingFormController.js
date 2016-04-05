cd0b112d8c6995dd12d27e790dae9478b01ad824
jnbohrer <jbohrer1@mac.com>
2016-04-04 20:14:40 -0500

    Added empty field checking in controller

--- app/example/scripts/SendingFormController.js
+++ app/example/scripts/SendingFormController.js
@@ -7,7 +7,17 @@ angular
     this.hello = "some other string";
 
     this.submit = function (){
-      //on click or on submit function
+      
+      // check for empty fields
+      if ($scope.master.length == null || $scope.master.length == "") {
+        supersonic.logger.debug("Empty length field");
+        return;
+      }
+      if ($scope.master.location == null || $scope.master.location == "") {
+        supersonic.logger.debug("Empty location field");
+        return;
+      }
+      
       supersonic.logger.debug($scope.master);
     };
   });
