62a3a988256498a776b2c6acd50ea0e7f3d4aceb
grygoree <GregoryLeung2016@u.northwestern.edu>
2016-04-04 19:32:26 -0500

    Form sending screwing around

--- app/example/scripts/SendingFormController.js
+++ app/example/scripts/SendingFormController.js
@@ -4,5 +4,8 @@ angular
 
     $scope.navbarTitle = "Learn More";
     this.hello = "some other string";
-
+    this.submit = function (){
+      //on click or on submit function
+      supersonic.logger.debug("Submitted form");
+    };
   });
