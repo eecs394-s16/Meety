80727337443e2c9353969755d777d674f9e4a9d5
Junjie Li <jj.muyang@gmail.com>
2016-04-05 21:02:48 -0500

    href-unsolved

--- app/example/scripts/MeetingController.js
+++ app/example/scripts/MeetingController.js
@@ -3,9 +3,9 @@ angular
   .controller('MeetingController', function($scope, supersonic) {
     $scope.testLoad = "LOADED";
 
-    $scope.meetings = [{purpose: "Website Breakdown", time: "5 pm", location: "Room 1", href: ""},
-                       {purpose: "Genaral Meeting", time: "7 pm", location: "Room 2", href: ""},
-                       {purpose: "Tech Meeting", time: "8 pm", location: "Room 3", href: ""}];
+    $scope.meetings = [{purpose: "Website Breakdown", time: "5 pm", location: "Room 1", href: "rsvp.html"},
+                       {purpose: "Genaral Meeting", time: "7 pm", location: "Room 2", href: "rsvp.html"},
+                       {purpose: "Tech Meeting", time: "8 pm", location: "Room 3", href: "rsvp.html"}];
 
 
     supersonic.logger.debug($scope.meetings);
