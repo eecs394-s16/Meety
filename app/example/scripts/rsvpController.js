8e6648a3c925828932d219d15f6fb3a07fbaf455
grygoree <GregoryLeung2016@u.northwestern.edu>
2016-04-06 18:42:33 -0500

    Started adding angular functionality to meeting response

--- app/example/scripts/rsvpController.js
+++ app/example/scripts/rsvpController.js
@@ -1,12 +1,19 @@
 angular
   .module('example')
   .controller('rsvpController', function($scope, supersonic) {
-  	$scope.master = {};
 
     $scope.navbarTitle = "Learn More";
 
     $scope.meetingSpecs = {hour: "5",minutes: "30",ampm: "PM",length: "tree fitty",location: "here",purpose: "urgent things"};
 
     $scope.somedummy = [{name: "Yoahne",attend: true},{name: "gergap",attend: false}];
+
+    $scope.acceptMeeting = function(){
+      supersonic.logger.debug("Accept meeting!");
+    };
+
+    $scope.rejectMeeting = function(){
+      supersonic.logger.debug("Reject meeting!");
+    };
 
   });
