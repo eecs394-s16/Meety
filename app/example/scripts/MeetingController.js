2e7a9fed2dab0e00ecea6e709a2c9e4484545d85
Ben Meline <benmeline@gmail.com>
2016-04-06 22:00:10 -0500

    Sync meetings and attendees

--- app/example/scripts/MeetingController.js
+++ app/example/scripts/MeetingController.js
@@ -1,7 +1,6 @@
 angular
   .module('example')
-  .controller('MeetingController', function($scope, supersonic) {
-    $scope.meetings = [{purpose: "Website Breakdown", time: "5 pm", location: "Room 1", href: "rsvp.html"},
-                       {purpose: "Genaral Meeting", time: "7 pm", location: "Room 2", href: "rsvp.html"},
-                       {purpose: "Tech Meeting", time: "8 pm", location: "Room 3", href: "rsvp.html"}];
+  .controller('MeetingController', function($scope, supersonic, $firebaseArray) {
+    var ref = new Firebase("https://glaring-fire-5657.firebaseio.com/meetings");
+    $scope.meetings = $firebaseArray(ref);
   });
