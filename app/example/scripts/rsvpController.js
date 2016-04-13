7df68e603a82fbe665ef81f483e0fc7a626a67f6
Junjie Li <jj.muyang@gmail.com>
2016-04-12 20:17:35 -0500

    nothing

--- app/example/scripts/rsvpController.js
+++ app/example/scripts/rsvpController.js
@@ -1,21 +1,23 @@
 angular
   .module('example')
   .controller('rsvpController', function($scope, supersonic, meetingService) {
+
     //Readability constants
     const buttonStatusEnum = {
       UNCONFIRMED: 0,
       ACCEPTED: 1,
       REJECTED: 2
-    }
+    };
 
     $scope.buttonStatus = buttonStatusEnum.UNCONFIRMED;
 
-    $scope.meetingSpecs = meetingService.find(steroids.view.params.id);
+   $scope.meetingSpecs = meetingService.find(steroids.view.params.id);
 
     $scope.acceptMeeting = function(newName){
       supersonic.logger.debug("Accept meeting!");
       if (newName) {
         $scope.meetingSpecs.attendees.push({ name: newName, attend: true });
+
 	      $scope.buttonStatus = buttonStatusEnum.ACCEPTED;
       }
     };
@@ -24,7 +26,9 @@ angular
       supersonic.logger.debug("Reject meeting!");
       if (newName) {
         $scope.meetingSpecs.attendees.push({ name: newName, attend: false });
+
 	      $scope.buttonStatus = buttonStatusEnum.REJECTED;
       }
     };
+
   });
