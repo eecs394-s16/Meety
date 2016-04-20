f5dc95cb8c858c96b20183e7b168c1d51615a2c0
grygoree <GregoryLeung2016@u.northwestern.edu>
2016-04-19 20:42:53 -0500

    All thats left to do is validation on login and register

--- app/meety/scripts/UserCtrl.js
+++ app/meety/scripts/UserCtrl.js
@@ -1,9 +1,16 @@
 angular
   .module('meety')
   .controller('UserCtrl', function($scope, supersonic) {
-  	$scope.toggledView = "login";
-  	$scope.switchView = function(view) {
-  		$scope.toggledView = view;
-  		supersonic.logger.debug(view);
-  	};
+    $scope.toggledView = "login";
+    $scope.switchView = function(view) {
+      $scope.toggledView = view;
+    };
+    $scope.registerInputs = {};
+    $scope.loginInputs = {};
+    $scope.login = function(input) {
+      //validate the inputs
+    };
+    $scope.register = function(input) {
+      //validate the inputs
+    };
   });
