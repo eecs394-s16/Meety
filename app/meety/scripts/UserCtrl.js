0d76b8e47a9d7fbf25ecb798de7109f43be31269
grygoree <GregoryLeung2016@u.northwestern.edu>
2016-04-19 19:58:48 -0500

    Changed switcher to a button

--- app/meety/scripts/UserCtrl.js
+++ app/meety/scripts/UserCtrl.js
@@ -1,4 +1,9 @@
 angular
   .module('meety')
   .controller('UserCtrl', function($scope, supersonic) {
+  	$scope.toggledView = "login";
+  	$scope.switchView = function(view) {
+  		$scope.toggledView = view;
+  		supersonic.logger.debug(view);
+  	};
   });
