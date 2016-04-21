74237cdc5a6df86a190c33995d88883bd319914b
Ben Meline <benmeline@gmail.com>
2016-04-21 17:47:34 -0500

    Check auth status in rsvp and meetings controllers

--- app/meety/scripts/MeetingCtrl.js
+++ app/meety/scripts/MeetingCtrl.js
@@ -3,7 +3,18 @@ angular
   .controller('MeetingCtrl', function($scope, supersonic, Meeting, $timeout) {
     $scope.meetings = [];
 
-    $timeout(function() {
-      $scope.meetings = Meeting.all();
-    }, 100);
+    $scope.getMeetings = function() {
+      var authData = JSON.parse(localStorage.getItem("authData"));
+      if (authData) {
+        $scope.meetings = Meeting.all();
+      } else {
+        $scope.meetings = [];
+      }
+    };
+    
+    $timeout($scope.getMeetings, 100);
+
+    supersonic.ui.views.current.whenVisible( function() {
+      $scope.getMeetings();
+    });
   });
