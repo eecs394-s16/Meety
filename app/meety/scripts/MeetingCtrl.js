f7ba2432c956bc23f79ed4bf0ee1e701985d736f
Junjie Li <jj.muyang@gmail.com>
2016-04-20 19:42:39 -0500

    edit and delete

--- app/meety/scripts/MeetingCtrl.js
+++ app/meety/scripts/MeetingCtrl.js
@@ -1,5 +1,18 @@
 angular
   .module('meety')
   .controller('MeetingCtrl', function($scope, supersonic, Meeting) {
+
+    $scope.enableEdit = false;
     $scope.meetings = Meeting.all();
+    $scope.editMeeting = function(){
+        $scope.enableEdit = !$scope.enableEdit;
+    };
+    $scope.removeMeeting = function(index) {
+        $scope.meetings.splice(index, 1);
+    };
+
+    $scope.editPermission = function(id) {
+        // return id == user.id
+        return true;
+    };
   });
