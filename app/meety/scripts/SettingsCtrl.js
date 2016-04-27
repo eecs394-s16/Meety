4aeb3313e6452a58fc65ffc984fdfb7d6bab9672
jnbohrer <jbohrer1@mac.com>
2016-04-27 18:37:41 -0500

    added confirmation for logout

--- app/meety/scripts/SettingsCtrl.js
+++ app/meety/scripts/SettingsCtrl.js
@@ -2,7 +2,16 @@ angular
   .module('meety')
   .controller('SettingsCtrl', function($scope, supersonic, Auth) {
     $scope.logoutUser = function() {
-      Auth.ref().$unauth();
-      supersonic.ui.initialView.show();
+
+      var options = {
+        buttonLabels: ["Yes","Cancel"]
+      };
+      // send confirmation alert when user tries to delete meeting
+      supersonic.ui.dialog.confirm("Are you sure you want to logout?", options).then(function(index) {
+        if (index == 0) {
+          Auth.ref().$unauth();
+          supersonic.ui.initialView.show();
+        }
+      });
     };
   });
